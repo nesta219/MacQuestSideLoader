2ffaf928b23edd106507720fd3814c45968b55ed
Mike Nesta <nesta219@gmail.com>
2019-05-24 20:14:08 -0400

    use better bash shebang

--- bundler.sh
+++ bundler.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/bash
+#!/usr/bin/env bash
 
 
 zip -r MacQuestSideLoader.zip platform-tools/ READ_ME.txt sideload-app.sh
--- sideload-app.sh
+++ sideload-app.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/bash
+#!/usr/bin/env bash
 
 filename=`ls | grep apk`
 
