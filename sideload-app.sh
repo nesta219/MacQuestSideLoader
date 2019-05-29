497329aadb2123af6da9bc67bc9baac57e02b7b6
Mike Nesta <nesta219@gmail.com>
2019-05-29 08:46:05 -0400

    get the directory right

--- sideload-app.sh
+++ sideload-app.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 filename=`ls | grep apk`
 
 echo Installing $filename
 
-platform-tools/adb install -r $filename
+$DIR/platform-tools/adb install -r $filename
